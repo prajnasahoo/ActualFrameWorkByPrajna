c0da987c77613a36a0d5b4b41f38acb0d6efddf1
lenovo <lenovo@sonalimohaptra@gmail.com>
2025-04-02 09:02:43 +0530

    sharing

--- FrameWorkByPrajna/src/test/java/contactTest/CreateContact.java
+++ FrameWorkByPrajna/src/test/java/contactTest/CreateContact.java
@@ -3,8 +3,7 @@ package contactTest;
 public class CreateContact {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("contact details");
 	}
 
 }
